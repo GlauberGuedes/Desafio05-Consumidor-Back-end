e3e128a2b1895c77f12a5bc7e5be1e02a1f91299
Bruno Sousa <brunodsousa@outlook.com>
2021-08-11 07:27:31 -0300

    login e middleware completos

---
+++ src/filtros/verificarToken.js
@@ -0,0 +1,34 @@
+const knex = require('../conexao');
+const jwt = require('jsonwebtoken');
+const senhaHash = require('../senhaHash');
+
+const verificarToken = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json('Não autorizado.');
+  }
+
+  try {
+    const token = authorization.replace('Bearer ', '').trim();
+
+    const { id } = jwt.verify(token, senhaHash);
+
+    const consumidorExiste = await knex('consumidor').where({ id }).first();
+
+    if (!consumidorExiste) {
+      return res.status(404).json('Usuário não encontrado');
+    }
+
+    const { senha, ...consumidor } = consumidorExiste;
+    
+
+    req.consumidor = consumidor;
+    
+    next();
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+module.exports = verificarToken;
---
+++ src/validacoes/validacaoLoginConsumidor.js
@@ -0,0 +1,28 @@
+function validarLogin(email, senha) {
+    if (!email) {
+      return "O campo 'email' é obrigatório.";
+    }
+
+    if (typeof email !== 'string') {
+      return "O campo 'email' precisa ser preenchido com um texto.";
+    }
+
+    if (!email.trim()) {
+      return "O campo 'email' é obrigatório.";
+    }
+
+    if (!email.includes("@")) {
+      return "O campo 'email' está escrito de forma incorreta.";
+    }
+
+    const indice = email.indexOf("@");
+    if (!email.includes(".", indice)) {
+      return "O campo 'email' está escrito de forma incorreta.";
+    }
+    
+    if (!senha) {
+      return "O campo 'senha' é obrigatório.";
+    }
+  }
+  
+  module.exports = validarLogin;
