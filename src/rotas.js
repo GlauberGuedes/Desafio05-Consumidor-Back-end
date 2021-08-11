d22142c5085a435a0d68fea26b5ed42d71a3892a
glauberguedes <glauber-94@hotmail.com>
2021-08-10 22:42:21 -0300

    cadastro do consumidor pronto

--- src/rotas.js
+++ src/rotas.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const rotas = express();
+const consumidor = require("./controladores/consumidor");
 
-
-
+rotas.post('/consumidor', consumidor.cadastrarConsumidor);
 
 module.exports = rotas;
---
+++ src/validacoes/validacaoCadastroConsumidor.js
@@ -0,0 +1,61 @@
+function validarConsumidor(
+  nome,
+  email,
+  telefone,
+  senha
+) {
+  if (!nome) {
+    return "O campo 'nome' é obrigatório.";
+  }
+  if (typeof nome !== "string") {
+    return "O campo 'nome' precisa ser preenchido com um texto.";
+  }
+  if (!nome.trim()) {
+    return "O campo 'nome' é obrigatório";
+  }
+  if (nome.length > 100) {
+    return "O campo 'nome' deve conter, no máximo, 100 caracteres.";
+  }
+  if (!email) {
+    return "O campo 'email' é obrigatório.";
+  }
+  if (typeof email !== "string") {
+    return "O campo 'email' precisa ser preenchido com um texto.";
+  }
+  if (!email.trim()) {
+    return "O campo 'email' é obrigatório.";
+  }
+  if (email.length > 100) {
+    return "O campo 'email' deve conter, no máximo, 100 caracteres.";
+  }
+  if (!email.includes("@")) {
+    return "O campo 'email' está incorreto.";
+  }
+  const indice = email.indexOf("@");
+  if (!email.includes(".", indice)) {
+    return "O campo 'email' está incorreto.";
+  }
+  if(!telefone) {
+    return "O campo 'telefone' é obrigatório.";
+  }
+  if(!Number(telefone)) {
+    return "O campo 'telefone' tem que ser um número.";
+  }
+  if (String(telefone).includes(".")) {
+    return "O campo 'telefone' tem que ser um número válido.";
+  }
+  if (!senha) {
+    return "O campo 'senha' é obrigatório.";
+  }
+  if (typeof senha !== "string") {
+    return "O campo 'senha' precisa ser preenchido com um texto.";
+  }
+  if (!senha.trim()) {
+    return "O campo 'senha' é obrigatório.";
+  }
+  if (senha.length < 6) {
+    return "O campo 'senha' deve conter, no mínimo, 6 caracteres.";
+  }
+}
+
+module.exports = validarConsumidor;
