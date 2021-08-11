d158ca6309c7d3fa475ee0e29acba2d7ea5da932
Bruno Sousa <brunodsousa@outlook.com>
2021-08-11 07:57:59 -0300

    endpoint de listar restaurantes completo

--- src/rotas.js
+++ src/rotas.js
@@ -3,10 +3,13 @@ const rotas = express();
 const consumidor = require("./controladores/consumidor");
 const login = require('./controladores/login');
 const verificarToken = require('./filtros/verificarToken');
+const restaurantes = require('./controladores/restaurantes');
 
 rotas.post('/consumidor', consumidor.cadastrarConsumidor);
 rotas.post('/login', login.loginConsumidor);
 
 rotas.use(verificarToken);
+
+rotas.get('/restaurantes', restaurantes.listarRestaurantes);
 
 module.exports = rotas;
