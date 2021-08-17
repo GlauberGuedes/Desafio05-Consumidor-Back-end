d3da37ccf5239abc873cafe017b6bd8912344ecb
Bruno Sousa <brunodsousa@outlook.com>
2021-08-16 23:06:36 -0300

    listar produtos de um restaurante

--- src/rotas.js
+++ src/rotas.js
@@ -11,5 +11,6 @@ rotas.post('/login', login.loginConsumidor);
 rotas.use(verificarToken);
 
 rotas.get('/restaurantes', restaurantes.listarRestaurantes);
+rotas.get('/restaurantes/:id', restaurantes.listarProdutos);
 
 module.exports = rotas;
