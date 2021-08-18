23d78e58389bf1fa3b58144fd2035d70f095fab3
Bruno Sousa <brunodsousa@outlook.com>
2021-08-18 14:42:10 -0300

    correção do retorno do endpoint listarProdutos

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -6,10 +6,6 @@ async function listarProdutos(req, res) {
     try {
         const produtos = await knex('produto').where({ restaurante_id: id });
 
-        if (produtos.length === 0) {
-            return res.status(400).json('Não há produtos cadastrados neste restaurante.');
-        }
-
         return res.status(200).json(produtos);
     } catch (error) {
         return res.status(400).json(error.message);
