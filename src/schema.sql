45761bfcfaffc133a4c7c5889c39fef7cccfe2ee
Bruno Sousa <brunodsousa@outlook.com>
2021-08-27 15:21:21 -0300

    Atualizando schema.sql

--- src/schema.sql
+++ src/schema.sql
@@ -18,9 +18,11 @@ CREATE TABLE pedido (
 	id SERIAL NOT NULL PRIMARY KEY,
   	consumidor_id INTEGER NOT NULL REFERENCES consumidor(id),
   	restaurante_id INTEGER NOT NULL REFERENCES restaurante(id),
-		subtotal INTEGER NOT NULL,
+	subtotal INTEGER NOT NULL,
   	taxa_de_entrega INTEGER NOT NULL DEFAULT 0,
-		valor_total INTEGER NOT NULL
+	valor_total INTEGER NOT NULL,
+	saiu_para_entrega BOOLEAN DEFAULT false,
+	entregue BOOLEAN DEFAULT false
 );
 
 CREATE TABLE itens_do_pedido (
